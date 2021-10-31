3fee882216df0744bfaf8121ed0cda7b7a489c5a
Volha <48136678+Ramaniuk@users.noreply.github.com>
2021-10-30 20:07:39 -0700

    Update test_items.py

--- test_items/test_items.py
+++ test_items/test_items.py
@@ -6,8 +6,8 @@ import time
 def test_find_the_button(browser, language):
     link = f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/"
     browser.get(link)
-    time.sleep(5)
+    time.sleep(30)
     button = browser.find_elements_by_css_selector(".btn-add-to-basket")
 
-    assert len(button) == 1, '!!! :((( !!!'
+    assert len(button) == 1, '!!! Error !!!'
 
